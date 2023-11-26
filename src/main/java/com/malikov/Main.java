0b0daf1075b225592831fb9432cb5490c1931bd7
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2023-11-26 15:36:14 +0300

    Created Cinema-System class

--- src/main/java/com/malikov/Main.java
+++ src/main/java/com/malikov/Main.java
@@ -2,6 +2,19 @@ package com.malikov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cinema cinema = new Cinema(5, 10, 20);
+        cinema.initializeSeats();
+
+        cinema.bookSeats(1, 3, new int[]{4, 5, 6});
+        cinema.bookSeats(1, 3, new int[]{5, 6});
+
+        cinema.printSeatingArrangement(1);
+
+        cinema.cancelBooking(1, 3, new int[]{5, 6});
+
+        cinema.printSeatingArrangement(1);
+
+        boolean availability = cinema.checkAvailability(1, 3);
+        System.out.println("Доступність 3 послідовних місць в залі 1: " + availability);
     }
 }
